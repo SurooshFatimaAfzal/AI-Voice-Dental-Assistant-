5a996badfcbdf2ef9b3c3b06cb19c8d048e6697e
SurooshFatimaAfzal <surooshfatima@gmail.com>
2025-10-14 20:23:47 +0500

    Made Components

--- dentwise/src/app/page.tsx
+++ dentwise/src/app/page.tsx
@@ -1,18 +1,22 @@
-import { SignedIn,SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
-import Image from "next/image";
-
+import Header from "@/components/Landing/Header";
+import Hero from "@/components/Landing/Hero";
+import HowItWorks from "@/components/Landing/HowItWorks";
+import WhatToAsk from "@/components/Landing/WhatToAsk";
+import PricingSection from "@/components/Landing/PricingSection";
+import CTA from "@/components/Landing/CTA";
+import Footer from "@/components/Landing/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <h1> Home Page </h1>
-      <SignedOut>
-      <SignUpButton mode="modal"> Sign Up </SignUpButton>
-      </SignedOut>
+    <div className="min-h-screen bg-background">
+      <Header/>
+      <Hero/>
+      <HowItWorks/>
+      <WhatToAsk/>
+      <PricingSection/>
+      <CTA/>
+      <Footer/>
 
-      <SignedIn>
-        <SignOutButton>Logout</SignOutButton>
-      </SignedIn>
     </div>
   );
 }
